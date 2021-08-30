2a71ac77e6e6876e48a95cd5a009b78c0f10f57e
Colton Henderson <coltonhenderson12@hotmail.com>
2021-08-30 16:55:01 -0500

    First Commit
    Tank is working with enemies working

---
+++ Assets/Scripts/SpeedIncrease.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedIncrease : CollectibleBase
+{
+    [SerializeField] float _speedAmount = .5f;
+
+    protected override void Collect(Player player)
+    {
+        //pull motor controlelr from the player
+        TankController controller = player.GetComponent<TankController>();
+        if(controller != null)
+        {
+            controller.MoveSpeed += _speedAmount;
+        }
+    }
+
+    protected override void Movement(Rigidbody rb)
+    {
+        //calculate rotation
+        Quaternion turnOffset = Quaternion.Euler(MovementSpeed, MovementSpeed, MovementSpeed);
+    }
+}
